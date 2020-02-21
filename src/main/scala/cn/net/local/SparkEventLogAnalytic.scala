12a36c3cce3d3764d86e81666db9842feb25030f
Hongjiang Zhang <hongjizhang@ebay.com>
2020-02-21 23:03:03 +0800

    add support for task metrics

--- src/main/scala/cn/net/local/SparkEventLogAnalytic.scala
+++ src/main/scala/cn/net/local/SparkEventLogAnalytic.scala
@@ -2,18 +2,45 @@ package cn.net.local
 
 import org.apache.commons.io.FilenameUtils
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.kohsuke.args4j.{CmdLineException, CmdLineParser, Option}
+
+import scala.collection.JavaConverters._
+class ArgumentsParser{
+  @Option(name="-i", aliases=Array("--input-json-file"), usage="Specify the json file")
+  var inputJsonFile: String = null
+
+  @Option(name="-o", aliases=Array("--output-dir"), usage="Specify the output directory")
+  var outputDir: String = null
+
+  @Option(name="-s", aliases=Array("--stage"), usage="Generate stage statistics, default is true")
+  var stageStat: Boolean = true
+
+  @Option(name="-t", aliases=Array("--task"), usage="Generate task statistics, default is false")
+  var taskStat: Boolean = false
+
+  def doParse(arguments: Array[String]): Unit = {
+    val parser = new CmdLineParser(this)
+    if (arguments.length < 1) {
+      parser.printUsage(System.out)
+      System.exit(-1)
+    }
+    try {
+      parser.parseArgument(arguments.toList.asJava)
+    }
+    catch {
+      case clEx: CmdLineException =>
+        System.out.println("ERROR: Unable to parse command-line options: " + clEx)
+    }
+  }
+}
 
 object SparkEventLogAnalytic {
-  def main(args: Array[String]): Unit = {
-    if (args.length != 2) {
-      System.err.println("Usage: <history_log_file> <analytic_out_dir>")
-      System.exit(1)
-    }
-    val jsonFile = args(0)
-    var outDir = args(1)
-    val outputFilename = FilenameUtils.getBaseName(jsonFile)
-    val spark = SparkSession.builder().appName("TestEventLog").getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
+  var argumentsParser: ArgumentsParser = null
+
+  private def stageInfo(spark: SparkSession): Unit = {
+    val jsonFile = argumentsParser.inputJsonFile
+    var outDir = argumentsParser.outputDir
+    val outputFilename = "stage" + FilenameUtils.getBaseName(jsonFile)
 
     val df = spark.read.json(jsonFile)
     val df2 = df.filter("Event='SparkListenerStageCompleted'").select("`Stage Info`.*")
@@ -23,6 +50,39 @@ object SparkEventLogAnalytic {
     val result = spark.sql("select Name, sum(Value) as value from t4 group by Name order by Name") //.show(100,false)
     result.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save(s"${outDir}/${outputFilename}")
     result.show(false)
+  }
+
+  private def taskInfo(spark: SparkSession): Unit = {
+    val jsonFile = argumentsParser.inputJsonFile
+    var outDir = argumentsParser.outputDir
+    val outputFilename = "task" + FilenameUtils.getBaseName(jsonFile)
+
+    val df = spark.read.json(jsonFile)
+    val df2 = df.filter("Event='SparkListenerTaskEnd'").select("Stage ID", "Task Info.*", "Task Metrics.*")
+    val result = df2.select("Input Metrics.*","Executor CPU Time","Finish Time","Locality")
+    result.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save(s"${outDir}/${outputFilename}")
+    result.show(false)
+  }
+
+  def main(args: Array[String]): Unit = {
+    argumentsParser = new ArgumentsParser()
+    argumentsParser.doParse(args)
+    if (!argumentsParser.stageStat && !argumentsParser.taskStat) {
+      return
+    }
+    val spark = SparkSession
+      .builder()
+      .appName("Spark event log analytics")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    if (argumentsParser.stageStat) {
+      stageInfo(spark)
+    }
+    if (argumentsParser.taskStat) {
+      taskInfo(spark)
+    }
     spark.close()
   }
 }
